34e266e3bd4df8cfe93f7af4b721e8471c144a65
Georgy-user <georgyt@mail.ru>
2024-06-05 22:44:36 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#Задача "Длина слова".
+a='proba'
+b=len(a)
+print(b)
+
+#Задача "Суммы и разности".
+first = 7
+second = 24
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+#Задача "Среднее арифметическое".
+first = 10
+second = 20
+third = 30
+mean = (first + second + third)/3
+print(mean)
+
+#Задача "Простые строчки".
+first_string = 'Вторник'
+second_string = "Понедельник, "
+print(second_string, first_string)
+
+#Задача "Сложная формула".
+a=1
+b=2
+c=3
+f=(a*b) + (a*c)
+d=f**3/2
+print(d)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
